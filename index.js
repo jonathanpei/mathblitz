22f7fcfc15768af537b06dad715b2f9db26eecb8
AmoleR <43121460+AmoleR@users.noreply.github.com>
2020-05-27 13:12:43 -0700

    Merge branch 'master' of https://github.com/RadiantCheddar/mathbowl

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var io = require('socket.io')(server);
 
 
 var gameNumber = 1;
-var games = [];
+var gameList = {};
 app.get('/login', function (req, res) {
   res.sendFile(__dirname+"/client/index.html");
 })
@@ -26,14 +26,15 @@ function addGames(socket){
 io.on('connection', function(socket) {
   socket.join("menu");
 
-  io.emit('addGames', games);
+  io.emit('addGames', gameList);
 
 
 
   socket.on('newGame', function(msg) {
 
-    games.push(gameNumber);
-    io.emit('addGames', games);
+    gameList[gameNumber+""]={name:msg};
+    console.log(gameList);
+    io.emit('addGames', gameList);
     socket.emit('joinedGame',0);
     socket.leave("menu");
     socket.join(gameNumber);
@@ -42,19 +43,16 @@ io.on('connection', function(socket) {
   });
   socket.on('message', function(msg) {
     var rooms = Object.keys(socket.rooms).filter(item => item!=socket.id);
-    console.log(rooms[0]);
     io.to(rooms[0]).emit('message', msg);
 
   });
   socket.on('joinGame', function(msg) {
-    console.log(msg);
     socket.emit('joinedGame',0);
     socket.leave("menu");
     socket.join(msg);
   });
   socket.on('leaveRoom',function(msg){
     var rooms = Object.keys(socket.rooms).filter(item => item!=socket.id);
-    console.log(rooms[0]);
     socket.leave(rooms[0]);
     socket.join("menu");
     socket.emit('leaveRoom',0);
