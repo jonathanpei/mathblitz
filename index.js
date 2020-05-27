31bb36104f31951846daeb5272c2dfd99cab277a
RadiantCheddar <jonathanpei2004@gmail.com>
2020-05-26 22:14:56 -0400

    change app

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ var server = http.Server(app);
 
 app.use(express.static('client'));
 
-server.listen(PORT, function() {
-  console.log('Chat server running');
-});
+
 
 var io = require('socket.io')(server);
 
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+var roomno = 1;
 io.on('connection', function(socket) {
-  socket.on('message', function(msg) {
-    io.emit('message', msg);
-  });
+   
+   //Increase roomno 2 clients are present in a room.
+   if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
+   socket.join("room-"+roomno);
+
+   //Send this event to everyone in the room.
+   io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
+})
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
 });
